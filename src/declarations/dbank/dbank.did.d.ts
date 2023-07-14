7b4a7c3173321bf2e7dc7f5c76ffc368531cb1ea
mahendraDew <mahendradewangan195@gmail.com>
2023-07-14 19:54:37 +0530

    Revert "inital commit"
    This reverts commit 9e91b13c4ecbffa10ae416fa2d3d24bcde3de9c4.

--- src/declarations/dbank/dbank.did.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Principal } from '@dfinity/principal';
-export interface _SERVICE {
-  'checkBalance' : () => Promise<number>,
-  'compound' : () => Promise<undefined>,
-  'topUp' : (arg_0: number) => Promise<undefined>,
-  'withdrawl' : (arg_0: number) => Promise<undefined>,
-}
